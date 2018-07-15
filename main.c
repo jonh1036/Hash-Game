b0a085d9697ba75a918cd92609d02dfcf2dd9d1f
jonh1036 <jonh1036@hotmail.com>
2018-07-15 13:20:50 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int x = 0, y = 0, round = 0;//Coordenadas da matriz e contador de rodadas
+char mat[3][3];//Criação da matriz
+
+int main(void) {
+    char tecla;
+    
+    
+    while(1){
+	
+    }
+    printf("\n\n");	
+	return 0;
+}
