4bd35ad1d8e8105e9b837e24eb2b01be4f990c89
jonh1036 <jonh1036@hotmail.com>
2018-07-15 14:46:51 -0300

    Create function cont()

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include "outros.h"
 
-int x = 0, y = 0, round = 0;//Coordenadas da matriz e contador de rodadas
+int x = 0, y = 0, round = 0, vitX = 0, vitO = 0;//Coordenadas da matriz e contador de rodadas
 char mat[3][3], tecla;//Criação da matriz e opção
 
 int main(void) {
     
 	inicializa();//Inicialização da Matriz
     while(1){
+    	if(round%2==0)
+    		printf("\n\nVez do jogador X");
+    	else
+    		printf("\n\nVez do jogador O");
     	printf("\n\nPara escolher, tecle <ENTER>");
 		tecla = getch();
 		system("cls");
--- outros.h
+++ outros.h
@@ -10,6 +10,7 @@ void direita();
 void esquerda();
 void baixo();
 void cima();
+int cont(int contX, int contO);
 
 extern char mat[3][3], tecla;
 extern int round, x, y;
@@ -43,31 +44,31 @@ void inicializa(){//Função que inicializa a matriz no início do programa
     mat[0][0] = ' ';
 }
 
-void direita(){
+void direita(){//Função que movimenta para a direita
 	if (x<2)
 		x++;
 	imprimir();
 }
 
-void esquerda(){
+void esquerda(){//Função que movimenta para a esquerda
 	if (x>0)
 		x--;
 	imprimir();
 }
 
-void baixo(){
+void baixo(){//Função que movimenta para baixo
 	if (y<2)
 		y++;
 	imprimir();
 }
 
-void cima(){
+void cima(){//Função que movimenta para cima
 	if (y>0)
 		y--;
 	imprimir();
 }
 
-void verifica(){
+void verifica(){//Função que verifica os casos de teste
 	switch(tecla){
 		case 13: escolha();//Cadastrar a escolha
 			break;
@@ -81,41 +82,46 @@ void verifica(){
 			break;
 		case 119: cima();//Movimentar para cima
 			break;
+		default:  imprimir();
+			break;
 	}
 }
 
-void escolha(){
+void escolha(){//Função que cadastra a escolha do jogador da vez
 	if(round%2==0 && tecla == 13)
 		mat[y][x] = 'X';
 	else if(round!=0 && tecla == 13)
 		mat[y][x] = 'O';
-		
+	imprimir();
 	round++;
 }
 
-void vitoria(){
+void vitoria(){//Função que verifica se o jogador que escolheu venceu
 	int i, j, contX = 0, contO = 0;
 	
-	for(i=0;i<3;i++){
+	for(i=0;i<3;i++){		//Verificação por linhas
 		for(j=0;j<3;j++){
 			if(mat[i][j] == 'X')
 				contX++;
 			else if(mat[i][j] == 'O')
 				contO++;
 		}
-		if(contX==3){
-			printf("\n\nVITORIA DO X");	
-			exit(0);
-		}
-		else if(contO==3){
-			printf("\n\nVITORIA DO O");	
-			exit(0);
-		}
-		else{
-			contX = 0;
-			contO = 0;
-		}
+		contX = cont(contX, contO);
+		contO = contX;
 	}
+	
+	for(i=0;i<3;i++){			//Verificação por colunas
+		for(j=0;j<3;j++){
+			if(mat[j][i] == 'X')
+				contX++;
+			else if(mat[j][i] == 'O')
+				contO++;
+		}
+		contX = cont(contX, contO);
+		contO = contX;
+	
+	}			
+	
 	if((mat[0][0] == 'X' && mat[1][1] == 'X' && mat[2][2] == 'X')||(mat[0][2] == 'X' && mat[1][1] == 'X' && mat[2][0] == 'X' )){
 		printf("\n\nVITORIA DO X");	
 		exit(0);
@@ -124,4 +130,18 @@ void vitoria(){
 		printf("\n\nVITORIA DO O");	
 		exit(0);
 	}
+}
+
+int cont(int contX, int contO){//Função que imprime a mensagem de vitória do jogador da vez
+	if(contX==3){
+		printf("\n\nVITORIA DO X");	
+		exit(0);
+	}
+	else if(contO==3){
+		printf("\n\nVITORIA DO O");	
+		exit(0);
+	}
+	else{
+		return 0;
+	}	
 }
