240abd86d731da5848af61f37672020f16bbc5f0
jonh1036 <jonh1036@hotmail.com>
2018-07-16 22:27:46 -0300

    Register name of players

--- main.c
+++ main.c
@@ -5,14 +5,21 @@ int x = 0, y = 0, round = 0, vitX = 0, vitO = 0;//Coordenadas da matriz e contad
 char mat[3][3], tecla;//Criação da matriz e opção
 
 int main(void) {
+    char jogador1[20], jogador2[20];
     
+    puts("\nEntre com o nome do jogador 1: ");
+    fgets(jogador1, 20, stdin);
+    puts("\nEntre com o nome do jogador 2: ");
+    fgets(jogador2, 20, stdin);
+    system("cls");
 	inicializa();//Inicialização da Matriz
     while(1){
     	if(round%2==0)
-    		printf("\n\nVez do jogador X");
+    		printf("\n\nVez de: %s",jogador1);
     	else
-    		printf("\n\nVez do jogador O");
-    	printf("\n\nPara escolher, tecle <ENTER>");
+    		printf("\n\nVez de: %s",jogador2);
+    		
+    	printf("\nPara escolher, tecle <ENTER>");
 		tecla = getch();
 		system("cls");
 		verifica(tecla);
